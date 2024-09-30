cd29f8cf7a814a804fd794cc017aba59aa035a73
akmalgt28 <48912530+akmalgt28@users.noreply.github.com>
2024-09-30 22:39:26 +0700

    Update balls.py

--- balls.py
+++ balls.py
@@ -14,4 +14,8 @@ Total buah yang dikumpulkan selama seminggu adalah 280
 """
 buah = [10, 20, 30, 40, 50, 60, 70]
 
-# lanjutkan code dibawah ini
+# Menghitung total buah
+total_buah = sum(buah)
+
+# Menampilkan hasil
+print(f"Total buah yang dikumpulkan selama seminggu adalah {total_buah}")
